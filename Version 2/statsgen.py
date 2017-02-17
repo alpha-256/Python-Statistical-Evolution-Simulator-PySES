56bf241b07cb51b6393c59ee594d4dfc5fe092a0
alpha-256 <changmaddubstepremixer@gmail.com>
2017-02-17 10:51:39 +0700

    damn

--- Version 2/statsgen.py
+++ Version 2/statsgen.py
@@ -3,37 +3,40 @@ import simpleai
 import math
 from random import randint as ri
 
-class genStats:
+#var Declare
+global population
+population = int(input("Population? >>> "))
 
-    #var Declare
-    global population
-    population = int(input("Population? >>> "))
+#All Data
+genLife = []
+genStrength = []
+genMutation = []
 
-    #All Data
-    genLife = []
-    genStrength = []
-    genMutation = []
+#Generate random popuulation data
+x = 0
+while x < population:
+    genLife.append(ri(1, 100))
+    genStrength.append(ri(1, 20))
+    genMutation.append(ri(1, 5))
+    x = x + 1
 
-    #Generate random popuulation data
+#Translate to str format
+genLife = str(genLife)
+genStrength = str(genStrength)
+genMutation = str(genMutation)
+
+#open data.txt for input
+with open("lifeData.txt", "w+") as lifeData :
+    lifeData.write(genLife)
+    lifeData.close()
+with open("strengthData.txt", "w+") as strengthData :
+    strengthData.write(genStrength)
+    strengthData.close()
+with open("mutationData.txt", "w+") as mutationData :
+    mutationData.write(genMutation)
+    mutationData.close()
+
+for char in genLife:
     x = 0
-    while x < population:
-        genLife.append(ri(1, 100))
-        genStrength.append(ri(1, 20))
-        genMutation.append(ri(1, 5))
-        x = x + 1
-
-    #Translate to str format
-    genLife = str(genLife)
-    genStrength = str(genStrength)
-    genMutation = str(genMutation)
-
-    #open data.txt for input
-    with open("lifeData.txt", "w+") as lifeData :
-        lifeData.write(genLife)
-        lifeData.close()
-    with open("strengthData.txt", "w+") as strengthData :
-        strengthData.write(genStrength)
-        strengthData.close()
-    with open("mutationData.txt", "w+") as mutationData :
-        mutationData.write(genMutation)
-        mutationData.close()
+    print(genLife[x])
+    x = x + 1
