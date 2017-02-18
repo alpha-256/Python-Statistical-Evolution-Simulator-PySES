17eca9b6a504845f669488b6e1ac1847e5c7ae12
alpha-256 <changmaddubstepremixer@gmail.com>
2017-02-18 08:52:37 +0700

    update

--- Version 2/statsgen.py
+++ Version 2/statsgen.py
@@ -11,32 +11,43 @@ population = int(input("Population? >>> "))
 genLife = []
 genStrength = []
 genMutation = []
+survivability = []
 
-#Generate random popuulation data
-x = 0
-while x < population:
-    genLife.append(ri(1, 100))
-    genStrength.append(ri(1, 20))
-    genMutation.append(ri(1, 5))
-    x = x + 1
-
-#Translate to str format
-genLife = str(genLife)
-genStrength = str(genStrength)
-genMutation = str(genMutation)
-
-#open data.txt for input
-with open("lifeData.txt", "w+") as lifeData :
-    lifeData.write(genLife)
-    lifeData.close()
-with open("strengthData.txt", "w+") as strengthData :
-    strengthData.write(genStrength)
-    strengthData.close()
-with open("mutationData.txt", "w+") as mutationData :
-    mutationData.write(genMutation)
-    mutationData.close()
-
-for char in genLife:
+def genData(genLife, genStrength, genMutation):
+    #Generate random popuulation data
     x = 0
-    print(genLife[x])
-    x = x + 1
+    while x < population:
+        genLife.append(ri(1, 100))
+        genStrength.append(ri(1, 20))
+        genMutation.append(ri(1, 5))
+        x = x + 1
+
+def printData(genLife, genStrength, genMutation, survivability):
+    counter = 0
+    while counter < population:
+        print(genLife[counter], "  ||  ", genStrength[counter], "    ||  ", genMutation[counter])
+        survivability.append( genLife[counter] + genStrength[counter])
+        print(survivability[counter])
+        counter = counter + 1
+
+
+def saveData(genLife, genStrength, genMutation):
+
+    #Translate to str format
+    genLife = str(genLife)
+    genStrength = str(genStrength)
+    genMutation = str(genMutation)
+
+    #open data.txt for input
+    with open("lifeData.txt", "w+") as lifeData :
+        lifeData.write(genLife)
+        lifeData.close()
+    with open("strengthData.txt", "w+") as strengthData :
+        strengthData.write(genStrength)
+        strengthData.close()
+    with open("mutationData.txt", "w+") as mutationData :
+        mutationData.write(genMutation)
+        mutationData.close()
+
+genData(genLife, genStrength, genMutation)
+printData(genLife, genStrength, genMutation, survivability)
