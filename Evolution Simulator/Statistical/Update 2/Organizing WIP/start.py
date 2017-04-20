809a38911443e758a7f5d9c0db313cbe958bf481
alpha-256 <changmaddubstepremixer@gmail.com>
2017-04-20 22:01:12 +0700

    update

--- Evolution Simulator/Statistical/Update 2/Organizing WIP/start.py
+++ Evolution Simulator/Statistical/Update 2/Organizing WIP/start.py
@@ -36,7 +36,7 @@ def divMax(fData, realData):
 
     pass
 
-def genDataSet(population=101):
+def genDataSet(population=51):
 
     for _ in range(population):
         genDat(fData)
@@ -46,10 +46,7 @@ def genDataSet(population=101):
     return
 
 def printDataSet(childData, rrData, survivors):
-
-
     for indexCounter in range(0,len(realData),1):
-
         rdPrint = realData[indexCounter]
         reproData = rrData[indexCounter]
         setIndex = round(rdPrint, 2)
@@ -65,16 +62,23 @@ def printDataSet(childData, rrData, survivors):
     print(survivors, "Parents Survived")
 
 
-#def dataSave():
-    #with open("relative fitness.txt", "a") as f:
-        #f.write()
+def writeData():
+    with open("relative fitness.txt", "a") as f:
+        """
+        Save reproduction rate for later generations of entity
+        """
+        f.write(childRR)
 
     #Survivor Counter
 
 if __name__ == "__main__":
     genDataSet()
+    print("\n")
     printDataSet(childData, rrData, survivors)
+    print("\n")
     print(childRR)
+    print("\n")
     print(childData)
+    print("\n")
     print("run the script as the following to be interactive")
     print("python -i run.py")
---
+++ Evolution Simulator/Statistical/Update 2/Organizing WIP/theory.py
@@ -0,0 +1,49 @@
+from random import randint as ri
+from random import choice as rc
+import math
+
+#Memory Que step 1
+memQue           =  []
+allEntities      =  []
+entityFitness    =  []
+mutationRate     =  []
+reproductiveRate =  []
+survivalPercent  =  []
+
+#population
+population = 5
+
+#binary DNA select
+binary = ["O", "I"]
+
+indexCounter = 0
+
+"""
+Check list
+
+*Reproduction Rate
+*Mutation Rate
+*Survival Rate
+*Chance of winning against everything
+"""
+
+#Entity Data Generation
+def createEntity(memQue, indexCounter, mutationRate, entityFitness, reproductiveRate, survivalPercent):
+    for x in range(8):
+        memQue.append(rc(binary))
+    survivalPercent = int(ri(0,50))
+    mutationRate = int(ri(0,100)) / 2
+    reproductiveRate = int(ri(0,4))
+    if not isinstance(entityFitness, type(None)):
+        entityFitness.append(survivalPercent * reproductiveRate)
+    print("\n")
+    print("Mutation Rate                :: ", mutationRate)
+    print("Binary DNA Sequence          :: ", ("".join(memQue)))
+    print("Fitness                      :: ", entityFitness[indexCounter])
+    print("Chances of Mitosis           :: ", reproductiveRate)
+    print("Percent of survivalbility    :: ", survivalPercent)
+    print("\n")
+    indexCounter = indexCounter + 1
+
+for population in range(10) :
+    createEntity(memQue, indexCounter, mutationRate, entityFitness, reproductiveRate, survivalPercent)
