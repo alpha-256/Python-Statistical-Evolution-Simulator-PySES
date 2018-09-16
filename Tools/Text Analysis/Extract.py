d1798cf15183afdc96a6230e6323734f7d5ad357
alpha-256 <changmaddubstepremixer@gmail.com>
2018-09-16 20:58:16 +0800

    Update 2018//9/16 20:58PM

--- Tools/Text Analysis/Extract.py
+++ Tools/Text Analysis/Extract.py
@@ -6,7 +6,7 @@ decide = []
 answer = []
 
 with open("notes.txt", "r") as data:
-    dataHolder = data.read()
+    dataHolder = "The Prologue My teenage life revolves around a mental illness which I dont know what it is called. The voices in my head one says to kill everyone who calls me a liar or a madman. The other one was my ex Angelina. Several more joined the party when I fell to depression after Angelina died. She bled to death. I live in a space station in the year 2048 after a worldwide nuclear war broke out between North Korea China and Iraq against the rest of the world. During the war a secret society of scientists and their families moved underground by 16 kilometers below the surface and developed nuclear fusion and a way to travel to space without the use of rockets."
     dataHolder = dataHolder.lower()
 
 wordlist = dataHolder.split()
@@ -15,7 +15,7 @@ wordfreq = []
 for w in wordlist:
     wordfreq.append(wordlist.count(w))
 
-print("String\n" + wordstring +"\n")
+#print("String\n" + wordstring +"\n")
 print("List\n" + str(wordlist) + "\n")
 print("Frequencies\n" + str(wordfreq) + "\n")
 print("Total words\n" + str(len(wordfreq)) + "\n")
@@ -30,9 +30,12 @@ def percentCal():
 def probabilityDist():
     for index in range(500):
         answer.append(str(rcs(wordlist, decide)))
+
 percentCal()
 probabilityDist()
+
 print(decide)
+
 for index in range(0,len(answer)):
     print(index)
     x0 = str(answer[index])
