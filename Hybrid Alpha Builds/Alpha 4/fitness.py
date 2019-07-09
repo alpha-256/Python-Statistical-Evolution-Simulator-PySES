c7cee16754a8f37a32828b934d4f46d067e1e848
alpha-256 <alpha-256@users.noreply.github.com>
2019-07-09 18:48:49 +0800

    Update fitness.py

--- Hybrid Alpha Builds/Alpha 4/fitness.py
+++ Hybrid Alpha Builds/Alpha 4/fitness.py
@@ -3,43 +3,60 @@ Calculate Relative fitness
 https://www.radford.edu/~rsheehy/GraphingDemo/fitness1.html
 """
 from random import randint as ri
-reproductiveRate = []
-survivalRate = []
-#SR: survivalRate * reproductiveRate; find max value for differential fitness
+RR = []
 SR = []
-rf = []
+#SR: survivalRate * reproductiveRate; find max value for differential fitness
+SxR = []
+RF = []
 counter = 0
 tempCount = 1
 
 def genData(population):
     for i in range(population):
-        reproductiveRate.append(ri(0,2))
-        survivalRate.append(ri(1,100))
+        RR.append(ri(0,2))
+        SR.append(ri(1,100))
+"""
+REDUNDANT FOR NOW!
 
 def equivRR():
     for n in reproductiveRate:
         relativeFitness = n/survivalRate
-        rf.append(relativeFitness)
+        RF.append(relativeFitness)
 
 def equivSR():
     for n in survivalRate:
         relativeFitness = n/reproductiveRate
-        rf.append(relativeFitness)
+        RF.append(relativeFitness)
+"""
 
 def notEquiv(population):
     for n in range(population):
         #SR: survivalRate * reproductiveRate; temporary holder/process
-        SR.append(survivalRate[n] * reproductiveRate[n])
-        relativeFitness = SR[n]/max(SR)
-        rf.append(relativeFitness)
+        SxR.append(SR[n] * RR[n])
+        if SxR[n] == 0:
+            relativeFitness = 0
+        else:
+            relativeFitness = SxR[n]/max(SR)
+        RF.append(relativeFitness)
 
 def showData():
     for n in range(population):
         print("Cell", n)
-        print("Reproductive Rate :", reproductiveRate[n])
-        print("Survival Rate :", survivalRate[n])
-        print("Relative Fitness :", rf[n])
+        print("Reproductive Rate :", RR[n])
+        print("Survival Rate :", SR[n])
+        print("Relative Fitness Decmial :", RF[n])
+        print("Relative Fitness Percent :", round(RF[n] * 100, 5))
+        print("################################################")
+
+def showDebug():
+    print("DEBUG")
+    print(RR)
+    print(SR)
+    print(RF)
+    print("################################################")
+
 population = int(input("Population >>> "))
 genData(population)
 notEquiv(population)
 showData()
+showDebug()
